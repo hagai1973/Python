3a399745792ac1a6ee3bf5f8ac04668a42c195b4
hagai-tregerman <hagai.tregerman@forcepoint.com>
2022-04-05 10:06:27 +0300

    8 days

---
+++ 8Day/L8_1.py
@@ -0,0 +1,18 @@
+import os
+
+#clear
+os.system('cls')
+
+def greet_with_name(name):
+    print ("Hi " + name)
+    print ("How do you do ? ")
+    print ("How is the weather today ?")
+
+def greet():
+    print ("Hi anyone" )
+    print ("How do you do ? ")
+    print ("How is the weather today ?")
+
+
+greet_with_name("John")
+
---
+++ 8Day/L8_2.py
@@ -0,0 +1,7 @@
+def greeting(name, location):
+    print(f"Hello: {name} You are in {location}")
+
+
+greeting(location="TLV", name="John" )
+greeting("Baruh", "London")
+greeting(name="", location="")
