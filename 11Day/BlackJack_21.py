b9f067b137f880c618f2f4c8f39c9cae05766363
Hagai Tregerman <hagai1973@gmail.com>
2022-04-12 00:12:48 +0300

    Update BlackJack_21.py

--- 11Day/BlackJack_21.py
+++ 11Day/BlackJack_21.py
@@ -3,10 +3,12 @@ import os
 
 os.system('cls')
 
+keep_check = True
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 player_1 = []
-the_house = []
+dealer = []
+
 
 def select_random_from_array(arr = cards):
     item = random.randint(0, len(arr) - 1)
@@ -21,18 +23,112 @@ def sum_list_cards(arr=player_1):
         sum += arr[i]
     return sum
 
+def replace_ace(arr = player_1):
+    sum_of_arr = 0
+    for i in range (len(arr) - 1):
+        sum_of_arr = sum_list_cards(arr)
+        if arr[i] == 11 and sum_of_arr > 21:
+            arr[i] = 1
+    return arr
+
+
+cards_sum_dealer = 0
+cards_sum_player1 = 0
+
+#first card for you
 get_card(player_1)
 print (f"player 1 card is:  {player_1[0]}")
-get_card(the_house)
-print (f"player 2 card is:  {the_house[0]}")
+
+#first card for dealer
+get_card(dealer)
+print (f"dealer card is:  {dealer[0]}")
+
+#second card for you
 get_card(player_1)
 cards_sum_player1 = sum_list_cards(player_1)
-print (f"your cards are:  {player_1}, the sum of the cards is: {cards_sum_player1}")
-to_contiue = input("Do you want to take another card: y/n \n ")
 
-if (to_contiue=="y"):
+
+if 11 in player_1:
+           player_1 = replace_ace(player_1)
+           cards_sum_player1 = sum_list_cards(player_1)
+
+print (f"player 1 second card is:  {player_1[-1]}, sum of the cards is: {cards_sum_player1}")
+
+
+to_contiue="y"
+
+if cards_sum_player1 == 21:
+    to_contiue = "n"
+    print (f"your cards are:  {player_1}, the final sum of yours cards is: {cards_sum_player1}, Lets see what the dealer do...")
+
+if to_contiue == "y":
+    to_contiue = input("Do you want to take another card: y/n \n ").lower()
+
+#loop of cards until stopping or exeeding 21
+while (to_contiue=="y") and (cards_sum_player1 < 21):
     get_card(player_1)
     cards_sum_player1 = sum_list_cards(player_1)
-    print (f"your cards are:  {player_1}, the sum of the cards is: {cards_sum_player1}")
+    # print (f"your cards are:  {player_1}, the sum of yours cards is: {cards_sum_player1}")
+    if cards_sum_player1 == 21:
+        to_contiue = "n"
+        # print (f"your cards are:  {player_1}, the final sum of yours cards is: {cards_sum_player1}, Lets see what the dealer do...")
+        break
+    if int(cards_sum_player1) < 21:
+        print (f"1. your cards are:  {player_1}, the sum of yours cards is: {cards_sum_player1}")
+        # to_contiue = input("Do you want to take another card: y/n \n ").lower()
+    else:
+        if 11 in player_1:
+           player_1 = replace_ace(player_1)
+        else:
+            to_contiue = "n"
+    cards_sum_player1 = sum_list_cards(player_1)
+    if cards_sum_player1 > 21:
+        break
+    else:
+        # print (f"your cards are 2:  {player_1}, the sum of yours cards is: {cards_sum_player1}")
+        to_contiue = input("Do you want to take another card: y/n \n ").lower()
+            
+if int(cards_sum_player1) > 21:
+    print (f"The final sum of yours cards is: {cards_sum_player1}, your cards {player_1},  You Lost !!!")
+    keep_check = False
+else:
+    print (f"your cards are:  {player_1}, the final sum of yours cards is: {cards_sum_player1}, Lets see what the dealer do...")
+    get_card(dealer)
+    print (f"dealer next card is:  {dealer[-1]}")
+    if 11 in dealer:
+            dealer = replace_ace(dealer)
+    cards_sum_dealer = sum_list_cards(dealer)
+    print (f"Dealer cards are:  {dealer}, the sum of dealer cards is: {cards_sum_dealer}")
+    
+    
+    while cards_sum_dealer < 17 or cards_sum_dealer < cards_sum_player1:
+        get_card(dealer)
+        cards_sum_dealer = sum_list_cards(dealer)
+        print (f"dealer next card is:  {dealer[-1]}, sum of dealers card is: {cards_sum_dealer}")
+        cards_sum_dealer = sum_list_cards(dealer)
+        if cards_sum_dealer > 21:
+            if 11 in dealer:
+                dealer = replace_ace(dealer)
+                cards_sum_dealer = sum_list_cards(dealer)
+            else:
+                print (f"Dealer cards are:  {dealer}, the sum of dealer cards is: {cards_sum_dealer}")
+        elif cards_sum_dealer < cards_sum_player1:
+            while (cards_sum_dealer < cards_sum_player1) and (cards_sum_dealer < 22):
+                get_card(dealer)
+                print (f"dealer next card is:  {dealer[-1]}")
+                cards_sum_dealer = sum_list_cards(dealer)
+                if cards_sum_dealer > 21:
+                        if 11 in dealer:
+                            dealer = replace_ace(dealer)
+                            cards_sum_dealer = sum_list_cards(dealer)
+                print (f"Dealer cards are:  {dealer}, the sum of dealer cards is: {cards_sum_dealer}")
+            
 
-
+if (cards_sum_dealer > cards_sum_player1) and (cards_sum_dealer < 22) and keep_check == True:
+    print (f"The final sum of yours cards is: {cards_sum_player1}, the final cards sum of the dealer is {cards_sum_dealer}, You Lost !!!")
+    keep_check = False
+elif cards_sum_dealer == cards_sum_player1 and  keep_check == True:
+    print (f"The final sum of yours cards is: {cards_sum_player1}, the final cards sum of the dealer is {cards_sum_dealer}, Draw !!!")
+    keep_check = False
+elif keep_check == True:
+    print (f"The final sum of yours cards is: {cards_sum_player1}, the final cards sum of the dealer is {cards_sum_dealer}, delar card are: {dealer} , You Win !!!")
